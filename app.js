92d84ce0e8cd8322d5000995b1532f32935c1806
Wilsonc7 <wolfc777@gmail.com>
2023-05-29 12:10:17 -0300

    generando error

--- app.js
+++ app.js
@@ -3,6 +3,6 @@ const { argv } = require('process')
 const argv=require ('process').argv
 let operacion = argv[2]
 let numeroA=argv[3]
-let numeroB=argv[4]
+let numeroB=argv[5]
 ///////////////////////////////////////
 
