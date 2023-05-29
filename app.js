6f1713c8033793c94fd6e9e1b2d740bacbf08bcc
Wilsonc7 <wolfc777@gmail.com>
2023-05-29 12:03:53 -0300

    agregar requires + argv

---
+++ app.js
@@ -0,0 +1,8 @@
+/////requires/////////////////////////
+const { argv } = require('process')
+argv=require ('process').argv
+operacion = argv[2]
+numeroA=argv[3]
+numerB=[4]
+///////////////////////////////////////
+
